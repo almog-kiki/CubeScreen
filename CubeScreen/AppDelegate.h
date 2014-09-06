e58e95bb8fb475d510bc6a4be0ec7a14b07f1b03
almog <almog_0@bitbucket.org>
2014-09-06 15:38:50 +0300

    Initial Commit

---
+++ CubeScreen/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CubeScreen
+//
+//  Created by kiki kalifa on 9/6/14.
+//  Copyright (c) 2014 kiki kalifa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
